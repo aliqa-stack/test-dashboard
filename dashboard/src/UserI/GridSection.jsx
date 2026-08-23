ffd8b2ad054e895f4342327b49c98e3bb64cc5e2
aliqa-stack <alikalmaliki51@gmail.com>
2026-08-23 19:29:41 +0700

    decorating grid section

--- dashboard/src/UserI/GridSection.jsx
+++ dashboard/src/UserI/GridSection.jsx
@@ -2,13 +2,38 @@ import React from 'react'
 
 const GridSection = () => {
   return (
-    <div>
+    <div className='flex justify-between justify-center p-4'>
+        <section className="grid lg:grid-cols-6 lg:grid-rows-4 gap-2">
+            <div className="lg:col-span-2 lg:row-span-4">
+              <div className='shadow-lg hover:shadow-xl hover:shadow-slate-500 bg-linear-to-br from-blue-400  to-orange-100 hover:from-blue-600 duration-500 transition ease-in-out   rounded-2xl h-68 w-72'>
+                <div className='p-4 '>
+                <span className='font-inter text-lg font-bold text-white italic'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
+                <p className='font-sans text-lg lg:text-sm text-slate-800'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veniam!</p>
+                </div>
+              </div>
+            </div>
+            <div className="lg:col-span-4 lg:row-span-2 lg:col-start-3">
+              <div className='bg-linear-to-br from-blue-400 to-orange-100 h-full rounded-2xl shadow-lg hover:shadow-xl hover:shadow-slate-400 duration-200 transition-all'>
+                <div className='p-4   '>
+                <span className='font-bold text-xl font-inter '>Lorem ipsum dolor sit amet consectetur.</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:col-span-2 lg:row-span-2 lg:col-start-3 lg:row-start-3">
+              <div className='bg-linear-to-br from-blue-400 to-orange-100 rounded-2xl h-32 shadow-lg hover:shadow-xl hover:shadow-slate-500 duration-400 transition ease-in-out '>
+              <div className='p-4'>
+                <span className='font-semibold text-xl font-inter '>Lorem ipsum dolor sit amet.</span>
+              </div>
+              </div>
 
-        <section className="grid grid-cols-6 grid-rows-4 gap-4">
-            <div className="col-span-2 row-span-4">1</div>
-            <div className="col-span-4 row-span-2 col-start-3">2</div>
-            <div className="col-span-2 row-span-2 col-start-3 row-start-3">4</div>
-            <div className="col-span-2 row-span-2 col-start-5 row-start-3">5</div>
+            </div>
+            <div className="lg:col-span-2 lg:row-span-2 lg:col-start-5 lg:row-start-3">
+              <div className=' bg-linear-to-br from-blue-400 to-orange-100 rounded-2xl h-32 shadow-lg shadow-lg hover:shadow-xl hover:shadow-slate-500 duration-400 transition ease-in-out  '>
+              <div className='p-4 '>
+                <span className='font-semibold text-xl font-inter'>Lorem ipsum dolor sit amet.</span>
+              </div>
+              </div>
+            </div>
         </section>
     
     </div>
